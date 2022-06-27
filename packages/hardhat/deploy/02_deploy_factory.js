7f59336ea1750b31d80b86bcc52141f11c0357a8
Arash <moosapour.arash@gmail.com>
2022-06-27 20:28:53 +0430

    hardhat cleanup

--- packages/hardhat/deploy/02_deploy_factory.js
+++ packages/hardhat/deploy/02_deploy_factory.js
@@ -1,15 +1,6 @@
 const {ethers} = require("hardhat");
 
 const localChainId = "31337";
-
-// const sleep = (ms) =>
-//   new Promise((r) =>
-//     setTimeout(() => {
-//       console.log(`waited for ${(ms / 1000).toFixed(3)} seconds`);
-//       r();
-//     }, ms)
-//   );
-
 module.exports = async ({getNamedAccounts, deployments, getChainId}) => {
   const {deploy} = deployments;
   const {deployer} = await getNamedAccounts();
@@ -26,65 +17,10 @@ module.exports = async ({getNamedAccounts, deployments, getChainId}) => {
     args: [
       auctionContract.address,
       verifierContract.address,
-      // "20854328272670001920775964005330023285873053800273121236978315039672155776763",
-      // "19084705740941453514771588581632038730939187249790695954077162624581946959904",
-      // 120, // biding period in seconds
-      // 120, // challenge duration
-      // verifierContract.address,
     ],
     log: true,
     waitConfirmations: 5,
   });
 
-  // Getting a previously deployed contract
-  const Auction = await ethers.getContract("Auction", deployer);
-  /*  await YourContract.setPurpose("Hello");
-  
-    To take ownership of yourContract using the ownable library uncomment next line and add the 
-    address you want to be the owner. 
-    // await yourContract.transferOwnership(YOUR_ADDRESS_HERE);
-
-    //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
-  */
-
-  /*
-  //If you want to send value to an address from the deployer
-  const deployerWallet = ethers.provider.getSigner()
-  await deployerWallet.sendTransaction({
-    to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
-    value: ethers.utils.parseEther("0.001")
-  })
-  */
-
-  /*
-  //If you want to send some ETH to a contract on deploy (make your constructor payable!)
-  const yourContract = await deploy("YourContract", [], {
-  value: ethers.utils.parseEther("0.05")
-  });
-  */
-
-  /*
-  //If you want to link a library into your contract:
-  // reference: https://github.com/austintgriffith/scaffold-eth/blob/using-libraries-example/packages/hardhat/scripts/deploy.js#L19
-  const yourContract = await deploy("YourContract", [], {}, {
-   LibraryName: **LibraryAddress**
-  });
-  */
-
-  // Verify from the command line by running `yarn verify`
-
-  // You can also Verify your contracts with Etherscan here...
-  // You don't want to verify on localhost
-  // try {
-  //   if (chainId !== localChainId) {
-  //     await run("verify:verify", {
-  //       address: YourContract.address,
-  //       contract: "contracts/YourContract.sol:YourContract",
-  //       constructorArguments: [],
-  //     });
-  //   }
-  // } catch (error) {
-  //   console.error(error);
-  // }
 };
 module.exports.tags = ["AuctionFactory"];
